d75d05209feae71a307b5d5f838d08bee08cb8f2
Farimah Nassiri <46692395+farimahnassiri@users.noreply.github.com>
2024-10-29 14:15:29 -0700

    init project

---
+++ src/components/Events/EditEvent.jsx
@@ -0,0 +1,110 @@
+import { Link, redirect, useNavigate, useParams, useSubmit, useNavigation } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
+import Modal from '../UI/Modal.jsx';
+import EventForm from './EventForm.jsx';
+import { fetchEvent, updateEvent, queryClient } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+
+export default function EditEvent() {
+  const navigate = useNavigate();
+  const { state } = useNavigation();
+  const submit = useSubmit();
+  const params = useParams();
+
+  const { data, isError, error } = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({signal}) => fetchEvent({signal, id: params.id}),
+    staleTime: 10000
+  });
+
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   onMutate: async (data) => {
+  //     const newEvent = data.event;
+  //     await queryClient.cancelQueries({queryKey: ['events', params.id]});
+
+  //     const previousEvent = queryClient.getQueryData(['events', params.id]);
+
+  //     queryClient.setQueryData(['events', params.id], newEvent);
+
+  //     return { previousEvent }
+  //   },
+  //   onError: (error, data, context) => {
+  //     queryClient.setQueryData(['events', params.id], context.previousEvent)
+  //   },
+  //   //this is here to ensure the data from backend and front are in sync, force the data to be fetched again
+  //   onSettled: () => {
+  //     queryClient.invalidateQueries(['events', params.id]);
+  //   }
+  // });
+
+  function handleSubmit(formData) {
+    //we are not sending an http request here
+    submit(formData, { method: 'PUT' });
+  }
+
+  function handleClose() {
+    navigate('../');
+  }
+
+  let content;
+
+  if (isError){
+    content = 
+    <>
+      <ErrorBlock
+        title="Failed to load event"
+        message={
+          error.info?.message ||
+          'Failed to load event. Please check you inputs and try again later.'
+        }
+      />
+      <div className="form-actions">
+        <Link to="../" className="button">
+          Okay
+        </Link>
+      </div>;
+    </>
+
+  }
+
+  if (data){
+    content = (
+      <EventForm inputData={data} onSubmit={handleSubmit}>
+        {state === 'submitting' ? (
+          <p>Sending data...</p>
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
+      </EventForm>
+    );
+  }
+
+  return (
+    <Modal onClose={handleClose}>{content}</Modal>
+  );
+}
+
+export function loader ({ params }){
+  // another way to fetch data, here with the help of react-router-dom
+  return queryClient.fetchQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({signal}) => fetchEvent({signal, id: params.id}),
+  });
+}
+//we're not using the optimistic update anymore
+export async function action({ request, params }){
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+  await updateEvent({ id: params.id, event: updatedEventData });
+  await queryClient.invalidateQueries(['events']);
+  return redirect('../');
+}
